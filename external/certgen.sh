99d94e3753cf0b290feabc9cd9dc82da1bfae62b
Jackson <me@jacksonisiah.pro>
2020-10-17 01:45:43 -0400

    Use actual certgen instead of old broken one

--- external/certgen.sh
+++ external/certgen.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 read -p "What's the name of your server?" name
-server_name=${name=Gulag}
+name=${name=Gulag}
 read -p "What country is this based in? (ISO country code)" location
-country_name=${location=CA}
+location=${location=CA}
 
 args="/CN=*.ppy.sh/O=$name/C=$location"
 openssl req -subj $args -new -newkey rsa:4096 -sha256 -days 36500 -nodes -x509 -keyout key.pem -out cert.pem
