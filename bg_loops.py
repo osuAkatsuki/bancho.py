b5fa93cd650aed3dbb0268829ee81cb12a8b16ff
cmyui <cmyuiosu@gmail.com>
2021-02-01 19:01:40 -0500

    clean main & abstract bg loops into their own file

---
+++ bg_loops.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import lzma
+import time
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from cmyui.osu import ReplayFrame
+from cmyui.discord import Webhook
+from cmyui.discord import Embed
+from cmyui import log, Ansi
+
+
+import packets
+from constants.gamemodes import GameMode
+from constants.privileges import Privileges
+from objects import glob
+from utils.misc import get_press_times
+
+if TYPE_CHECKING:
+    from objects.score import Score
+
+__all__ = ('donor_expiry', 'disconnect_ghosts', 'replay_detections')
+
+async def donor_expiry() -> None:
+    """Add new donation ranks & enqueue tasks to remove current ones."""
+    # TODO: this system can get quite a bit better; rather than just
+    # removing, it should rather update with the new perks (potentially
+    # a different tier, enqueued after their current perks).
+
+    async def rm_donor(userid: int, when: int):
+        if (delta := when - time.time()) >= 0:
+            await asyncio.sleep(delta)
+
+        p = await glob.players.get(id=userid, sql=True)
+
+        # TODO: perhaps make a `revoke_donor` method?
+        await p.remove_privs(Privileges.Donator)
+        await glob.db.execute(
+            'UPDATE users '
+            'SET donor_end = 0 '
+            'WHERE id = %s',
+            [p.id]
+        )
+
+        if p.online:
+            p.enqueue(packets.notification('Your supporter status has expired.'))
+
+        log(f"{p}'s supporter status has expired.", Ansi.MAGENTA)
+
+    # enqueue rm_donor for any supporter
+    # expiring in the next 30 days.
+    query = (
+        'SELECT id, donor_end FROM users '
+        'WHERE donor_end < DATE_ADD(NOW(), INTERVAL 30 DAY) '
+        'AND priv & 48' # 48 = Supporter | Premium
+    )
+
+    loop = asyncio.get_running_loop()
+
+    async for donation in glob.db.iterall(query, _dict=False):
+        loop.create_task(rm_donor(*donation))
+
+PING_TIMEOUT = 300000 // 1000 # defined by osu!
+async def disconnect_ghosts() -> None:
+    """Actively disconnect users above the
+       disconnection time threshold on the osu! server."""
+    players_lock = asyncio.Lock()
+
+    while True:
+        ctime = time.time()
+
+        async with players_lock:
+            for p in glob.players:
+                if ctime - p.last_recv_time > PING_TIMEOUT:
+                    await p.logout()
+
+        # run this indefinitely
+        await asyncio.sleep(PING_TIMEOUT // 3)
+
+# This function is currently pretty tiny and useless, but
+# will just continue to expand as more ideas come to mind.
+async def analyze_score(score: 'Score') -> None:
+    """Analyze a single score."""
+    player = score.player
+
+    # open & parse replay files frames
+    replay_file = REPLAYS_PATH / f'{score.id}.osr'
+    data = lzma.decompress(replay_file.read_bytes())
+
+    frames: list[ReplayFrame] = []
+
+    # ignore seed & blank line at end
+    for action in data.decode().split(',')[:-2]:
+        if frame := ReplayFrame.from_str(action):
+            frames.append(frame)
+
+    if score.mode.as_vanilla == GameMode.vn_taiko:
+        # calculate their average press times.
+        # NOTE: this does not currently take hit object
+        # type into account, making it completely unviable
+        # for any gamemode with holds. it's still relatively
+        # reliable for taiko though :D.
+
+        press_times = get_press_times(frames)
+        config = glob.config.surveillance['hitobj_low_presstimes']
+
+        cond = lambda pt: (sum(pt) / len(pt) < config['value']
+                           and len(pt) > config['min_presses'])
+
+        if any(map(cond, press_times.values())):
+            # at least one of the keys is under the
+            # minimum, log this occurence to Discord.
+            webhook_url = glob.config.webhooks['surveillance']
+            webhook = Webhook(url=webhook_url)
+
+            embed = Embed(
+                title = f'[{score.mode!r}] Abnormally low presstimes detected'
+            )
+
+            embed.set_author(
+                url = player.url,
+                name = player.name,
+                icon_url = player.avatar_url
+            )
+
+            # TODO: think of a way to organize a thumbnail into config?
+            thumb_url = 'https://akatsuki.pw/static/logos/logo.png'
+            embed.set_thumbnail(url=thumb_url)
+
+            for key, pt in press_times.items():
+                embed.add_field(
+                    name = f'Key: {key.name}',
+                    value = f'{sum(pt) / len(pt):.2f}ms' if pt else 'N/A',
+                    inline = True
+                )
+
+            webhook.add_embed(embed)
+            await webhook.post(glob.http)
+
+REPLAYS_PATH = Path.cwd() / '.data/osr'
+async def replay_detections() -> None:
+    """Actively run a background thread throughout gulag's
+       lifespan; it will pull replays determined as sketch
+       from a queue indefinitely."""
+    glob.sketchy_queue = asyncio.Queue() # cursed type hint fix
+    queue: asyncio.Queue['Score'] = glob.sketchy_queue
+
+    loop = asyncio.get_running_loop()
+
+    while score := await queue.get():
+        loop.create_task(analyze_score(score))
--- main.py
+++ main.py
@@ -9,24 +9,15 @@
 # certificate: https://akatsuki.pw/static/ca.crt
 
 import asyncio
-import lzma
 import os
-import time
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 import aiohttp
 import cmyui
 import datadog
 import orjson # go zoom
-from cmyui import Ansi
-from cmyui import log
-from cmyui.discord import Embed
-from cmyui.discord import Webhook
-from cmyui.osu import ReplayFrame
 
-import packets
-from constants.gamemodes import GameMode
+import bg_loops
 from constants.privileges import Privileges
 from objects import glob
 from objects.achievement import Achievement
@@ -34,29 +25,15 @@ from objects.channel import Channel
 from objects.clan import Clan
 from objects.match import MapPool
 from objects.player import Player
-from utils.misc import get_press_times
 from utils.updater import Updater
-
-if TYPE_CHECKING:
-    from objects.score import Score
 
 __all__ = ()
 
 # current version of gulag
 glob.version = cmyui.Version(3, 1, 9)
 
-async def on_start() -> None:
-    glob.http = aiohttp.ClientSession(json_serialize=orjson.dumps)
-
-    # connect to mysql
-    glob.db = cmyui.AsyncSQLPool()
-    await glob.db.connect(glob.config.mysql)
-
-    # run the sql updater
-    updater = Updater(glob.version)
-    await updater.run()
-    await updater.log_startup()
-
+async def setup_globals() -> None:
+    """Setup & cache many global variables from various sources."""
     # create our bot & append it to the global player list.
     # TODO: perhaps name & id should be fetched from SQL?
     #       or perhaps the bot shouldn't be in SQL at all?
@@ -65,7 +42,9 @@ async def on_start() -> None:
 
     glob.players.append(glob.bot)
 
-    # add all channels from db.
+    """ load data for global objects from sql. """
+
+    # fetch & cache all channels from sql.
     async for res in glob.db.iterall('SELECT * FROM channels'):
         glob.channels.append(Channel(
             name = res['name'],
@@ -75,7 +54,7 @@ async def on_start() -> None:
             auto_join = res['auto_join'] == 1
         ))
 
-    # add all mappools from db.
+    # fetch & cache all mappools from sql.
     async for res in glob.db.iterall('SELECT * FROM tourney_pools'):
         created_by = await glob.players.get(id=res['created_by'], sql=True)
         pool = MapPool(
@@ -88,14 +67,14 @@ async def on_start() -> None:
         await pool.maps_from_sql()
         glob.pools.append(pool)
 
-    # add all clans from db.
+    # fetch & cache all clans from sql.
     async for res in glob.db.iterall('SELECT * FROM clans'):
         clan = Clan(**res)
 
         await clan.members_from_sql()
         glob.clans.append(clan)
 
-    # add all achievements from db.
+    # fetch & cache all achievements from sql.
     async for res in glob.db.iterall('SELECT * FROM achievements'):
         # NOTE: achievement conditions are stored as
         # stringified python expressions in the database.
@@ -103,7 +82,7 @@ async def on_start() -> None:
         achievement = Achievement(**res, cond=condition)
         glob.achievements[res['mode']].append(achievement)
 
-    """ bmsubmit stuff
+    """ XXX: Unfinished code for beatmap submission.
     # get the latest set & map id offsets for custom maps.
     maps_res = await glob.db.fetch(
         'SELECT id, set_id FROM maps '
@@ -115,133 +94,36 @@ async def on_start() -> None:
         glob.gulag_maps = maps_res
     """
 
-    # add new donation ranks & enqueue tasks to remove current ones.
-    # TODO: this system can get quite a bit better; rather than just
-    # removing, it should rather update with the new perks (potentially
-    # a different tier, enqueued after their current perks).
+async def on_start() -> None:
+    """Called when the server begins serving connections."""
+    # retrieve a client session to use for http connections.
+    glob.http = aiohttp.ClientSession(json_serialize=orjson.dumps)
 
-    async def rm_donor(userid: int, when: int):
-        if (delta := when - time.time()) >= 0:
-            await asyncio.sleep(delta)
+    # retrieve a pool of connections to use for mysql interaction.
+    glob.db = cmyui.AsyncSQLPool()
+    await glob.db.connect(glob.config.mysql)
 
-        p = await glob.players.get(id=userid, sql=True)
+    # run the sql & submodule updater (uses http & db).
+    updater = Updater(glob.version)
+    await updater.run()
+    await updater.log_startup()
 
-        # TODO: perhaps make a `revoke_donor` method?
-        await p.remove_privs(Privileges.Donator)
-        await glob.db.execute(
-            'UPDATE users '
-            'SET donor_end = 0 '
-            'WHERE id = %s',
-            [p.id]
-        )
+    # cache many global objects from sql,
+    # such as channels, mappools, clans, etc.
+    await setup_globals()
 
-        if p.online:
-            p.enqueue(packets.notification('Your supporter status has expired.'))
+    # setup tasks for upcoming donor expiry dates.
+    await bg_loops.donor_expiry()
 
-        log(f"{p}'s supporter status has expired.", Ansi.MAGENTA)
-
-    # enqueue rm_donor for any supporter
-    # expiring in the next 30 days.
-    query = (
-        'SELECT id, donor_end FROM users '
-        'WHERE donor_end < DATE_ADD(NOW(), INTERVAL 30 DAY) '
-        'AND priv & 48' # 48 = Supporter | Premium
-    )
-
+    # setup a loop to kick inactive ghosted players.
     loop = asyncio.get_running_loop()
+    loop.create_task(bg_loops.disconnect_ghosts())
 
-    async for donation in glob.db.iterall(query, _dict=False):
-        loop.create_task(rm_donor(*donation))
-
-PING_TIMEOUT = 300000 // 1000
-async def disconnect_inactive() -> None:
-    """Actively disconnect users above the
-       disconnection time threshold on the osu! server."""
-    players_lock = asyncio.Lock()
-
-    while True:
-        ctime = time.time()
-
-        async with players_lock:
-            for p in glob.players:
-                if ctime - p.last_recv_time > PING_TIMEOUT:
-                    await p.logout()
-
-        # run this indefinitely
-        await asyncio.sleep(PING_TIMEOUT // 3)
-
-# This function is currently pretty tiny and useless, but
-# will just continue to expand as more ideas come to mind.
-async def analyze_score(score: 'Score') -> None:
-    """Analyze a single score."""
-    player = score.player
-
-    # open & parse replay files frames
-    replay_file = REPLAYS_PATH / f'{score.id}.osr'
-    data = lzma.decompress(replay_file.read_bytes())
-
-    frames: list[ReplayFrame] = []
-
-    # ignore seed & blank line at end
-    for action in data.decode().split(',')[:-2]:
-        if frame := ReplayFrame.from_str(action):
-            frames.append(frame)
-
-    if score.mode.as_vanilla == GameMode.vn_taiko:
-        # calculate their average press times.
-        # NOTE: this does not currently take hit object
-        # type into account, making it completely unviable
-        # for any gamemode with holds. it's still relatively
-        # reliable for taiko though :D.
-
-        press_times = get_press_times(frames)
-        config = glob.config.surveillance['hitobj_low_presstimes']
-
-        cond = lambda pt: (sum(pt) / len(pt) < config['value']
-                           and len(pt) > config['min_presses'])
-
-        if any(map(cond, press_times.values())):
-            # at least one of the keys is under the
-            # minimum, log this occurence to Discord.
-            webhook_url = glob.config.webhooks['surveillance']
-            webhook = Webhook(url=webhook_url)
-
-            embed = Embed(
-                title = f'[{score.mode!r}] Abnormally low presstimes detected'
-            )
-
-            embed.set_author(
-                url = player.url,
-                name = player.name,
-                icon_url = player.avatar_url
-            )
-
-            # TODO: think of a way to organize a thumbnail into config?
-            thumb_url = 'https://akatsuki.pw/static/logos/logo.png'
-            embed.set_thumbnail(url=thumb_url)
-
-            for key, pt in press_times.items():
-                embed.add_field(
-                    name = f'Key: {key.name}',
-                    value = f'{sum(pt) / len(pt):.2f}ms' if pt else 'N/A',
-                    inline = True
-                )
-
-            webhook.add_embed(embed)
-            await webhook.post(glob.http)
-
-REPLAYS_PATH = Path.cwd() / '.data/osr'
-async def run_detections() -> None:
-    """Actively run a background thread throughout gulag's
-       lifespan; it will pull replays determined as sketch
-       from a queue indefinitely."""
-    glob.sketchy_queue = asyncio.Queue() # cursed type hint fix
-    queue: asyncio.Queue['Score'] = glob.sketchy_queue
-
-    loop = asyncio.get_running_loop()
-
-    while score := await queue.get():
-        loop.create_task(analyze_score(score))
+    # if the surveillance webhook has a value, run
+    # automatic (still very primitive) detections on
+    # replays deemed by the server's configurable values.
+    if glob.config.webhooks['surveillance']:
+        loop.create_task(bg_loops.replay_detections())
 
 if __name__ == '__main__':
     # set cwd to /gulag.
@@ -255,20 +137,20 @@ if __name__ == '__main__':
         subdir = data_path / sub_dir
         subdir.mkdir(exist_ok=True)
 
+    # create a server object, which serves as a map of domains.
     app = cmyui.Server(name=f'gulag v{glob.version}',
                        gzip=4, verbose=glob.config.debug)
 
-    # add our domains & tasks
+    # add our endpoint's domains to the server;
+    # each may potentially hold many individual endpoints.
     from domains.cho import domain as cho_domain # c[e4-6]?.ppy.sh
     from domains.osu import domain as osu_domain # osu.ppy.sh
     from domains.ava import domain as ava_domain # a.ppy.sh
     app.add_domains({cho_domain, osu_domain, ava_domain})
-    app.add_tasks({on_start(), disconnect_inactive()})
 
-    # if surveillance webhook is enabled,
-    # run our auto-detection 'thread' in bg.
-    if glob.config.webhooks['surveillance']:
-        app.add_task(run_detections())
+    # enqueue a task to run once the
+    # server begins serving connections.
+    app.add_task(on_start())
 
     # support for https://datadoghq.com
     if all(glob.config.datadog.values()):
@@ -279,9 +161,12 @@ if __name__ == '__main__':
         glob.datadog = datadog.ThreadStats()
         glob.datadog.start(flush_interval=15)
 
-        # set some base values
+        # wipe any previous stats from the page.
         glob.datadog.gauge('gulag.online_players', 0)
     else:
         glob.datadog = None
 
-    app.run(glob.config.server_addr) # blocking call
+    # start up the server; this starts
+    # an event loop internally, using
+    # uvloop if it's installed.
+    app.run(glob.config.server_addr)
