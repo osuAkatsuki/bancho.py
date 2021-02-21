446cb71de5965a1e1742b0c7570194be3fdf858c
cmyui <cmyuiosu@gmail.com>
2021-02-21 00:49:11 -0500

    fix ap score ids and prepare for v3.2.1

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ __all__ = ()
 # current version of gulag
 # NOTE: this is used internally for the updater, it may be
 # worth reading through it's code before playing with it.
-glob.version = cmyui.Version(3, 2, 0)
+glob.version = cmyui.Version(3, 2, 1)
 
 async def setup_collections() -> None:
     """Setup & cache many global collections (mostly from sql)."""
