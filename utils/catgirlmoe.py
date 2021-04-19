e6dc63a3f0bbb81a908dedc3013c3430be021418
Atakku <atakkudev@gmail.com>
2021-04-19 21:23:37 +0200

    OOOOOOOOOOOOOOOOOOOOOO

--- utils/catgirlmoe.py
+++ utils/catgirlmoe.py
@@ -161,7 +161,9 @@ async def sendMatchPart(p: Player, m: Match):
 
 async def sendMatchComplete(slots: list[Slot], m: Match):
   print("bruh moment3")
-
+  scores, didnt_submit = await self.await_submissions(was_playing)
+  print(scores)
+  print(didnt_submit)
   #wh = Webhook(url=WEBHOOK, content="GAMER MOMENT, SOME BITCHES PLAYED A MATCH")
 
   #e = Embed(color=0xF44336)
