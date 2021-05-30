384d70245d0d2c3da14eca1a7b59a23aa266a818
cmyui <cmyuiosu@gmail.com>
2021-05-29 22:09:19 -0400

    update version (v3.3.6)

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ utils.misc.install_excepthook()
 # current version of gulag
 # NOTE: this is used internally for the updater, it may be
 # worth reading through it's code before playing with it.
-glob.version = cmyui.Version(3, 3, 5)
+glob.version = cmyui.Version(3, 3, 6)
 
 OPPAI_PATH = Path.cwd() / 'oppai-ng'
 GEOLOC_DB_FILE = Path.cwd() / 'ext/GeoLite2-City.mmdb'
