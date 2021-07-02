7d7f45ee0a7b76ae41805144cdadbef58d6490a3
cmyui <cmyuiosu@gmail.com>
2021-07-01 21:24:35 -0400

    update mitmproxy usage comment

--- tools/proxy.py
+++ tools/proxy.py
@@ -1,5 +1,7 @@
 # like budget wireshark for osu! server stuff
-# usage: `mitmdump -qs tools/proxy.py`
+# usage: enable http://localhost:8080 proxy in windows,
+#        (https://i.cmyui.xyz/DNnqifKHyBSA9X8NEHg.png)
+#        and run this with `mitmdump -qs tools/proxy.py`
 
 domain = 'cmyui.xyz' # XXX: put your domain here
 
