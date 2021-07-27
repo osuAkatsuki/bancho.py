fcd02a64f7165996ad84cd1a6bf80fc26099face
JKBGL <44818773+JKBGL@users.noreply.github.com>
2021-07-27 15:06:35 +0300

    Move include_loved to be only checked in if scope == best

--- domains/osu.py
+++ domains/osu.py
@@ -1683,17 +1683,18 @@ async def api_get_player_scores(conn: Connection) -> HTTPResponse:
             query.append('AND t.mods & %s != 0')
             params.append(mods)
 
-    include_loved = (
-        'include_loved' in conn.args and
-        conn.args['include_loved'] == '1'
-    )
-    
-    allowed_statuses = [2, 3]
-
-    if include_loved:
-        allowed_statuses.append(5)
 
     if scope == 'best':
+        include_loved = (
+            'include_loved' in conn.args and
+            conn.args['include_loved'] == '1'
+        )
+    
+        allowed_statuses = [2, 3]
+
+        if include_loved:
+            allowed_statuses.append(5)
+            
         query.append('AND t.status = 2 AND b.status IN %s')
         params.append(allowed_statuses)
         sort = 't.pp'
