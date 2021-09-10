b90146edc886ecc1e2675b56861e4c65333902db
Myroslav Sukhoy <belofost22@gmail.com>
2021-09-10 21:35:31 +0300

    Implemented peace-performance for non std modes. (#133)

--- domains/cho.py
+++ domains/cho.py
@@ -22,9 +22,10 @@ from cmyui.osu.oppai_ng import OppaiWrapper
 from cmyui.utils import magnitude_fmt_time
 from cmyui.web import Connection
 from cmyui.web import Domain
-from maniera.calculator import Maniera
 
 import packets
+from peace_performance_python.objects import Beatmap as PeaceMap
+from peace_performance_python.objects import Calculator
 import utils.misc
 from constants import commands
 from constants import regexes
@@ -1001,56 +1002,76 @@ class SendPrivateMessage(BasePacket):
                             # calculate pp for common generic values
                             pp_calc_st = time.time_ns()
 
-                            if mode_vn in (0, 1): # osu, taiko
-                                with OppaiWrapper('oppai-ng/liboppai.so') as ezpp:
-                                    # std & taiko, use oppai-ng to calc pp
-                                    if r_match['mods'] is not None:
-                                        # [1:] to remove leading whitespace
-                                        mods_str = r_match['mods'][1:]
-                                        mods = Mods.from_np(mods_str, mode_vn)
-                                        ezpp.set_mods(int(mods))
+                            if mode_vn in (0, 1, 2): # osu, taiko, catch
+                                if r_match['mods'] is not None:
+                                    # [1:] to remove leading whitespace
+                                    mods_str = r_match['mods'][1:]
+                                    mods = Mods.from_np(mods_str, mode_vn)
+                                else:
+                                    mods = None
+                                
+                                pp_values = [] # [(acc, pp), ...]
 
-                                    pp_values = [] # [(acc, pp), ...]
+                                if mode_vn == 1:
+                                    with OppaiWrapper('oppai-ng/liboppai.so') as ezpp:
+                                        if mods is not None:
+                                            ezpp.set_mods(int(mods))
+
+                                        for acc in glob.config.pp_cached_accs:
+                                            ezpp.set_accuracy_percent(acc)
+
+                                            ezpp.calculate(osu_file_path)
+
+                                            pp_values.append((acc, ezpp.get_pp()))
+                                else:
+                                    beatmap = PeaceMap(str(osu_file_path))
+                                    peace = Calculator()
+
+                                    if mods is not None:
+                                        peace.set_mods(int(mods))
+
+                                    peace.set_mode(mode_vn)
 
                                     for acc in glob.config.pp_cached_accs:
-                                        ezpp.set_accuracy_percent(acc)
+                                        peace.set_acc(acc)
 
-                                        ezpp.calculate(osu_file_path)
+                                        calc = peace.calculate(beatmap)
 
-                                        pp_values.append((acc, ezpp.get_pp()))
+                                        pp_values.append((acc, calc.pp))
 
-                                    resp_msg = ' | '.join([
-                                        f'{acc}%: {pp:,.2f}pp'
-                                        for acc, pp in pp_values
-                                    ])
-                            elif mode_vn == 2: # catch
-                                resp_msg = 'Gamemode not yet supported.'
+                                resp_msg = ' | '.join([
+                                    f'{acc}%: {pp:,.2f}pp'
+                                    for acc, pp in pp_values
+                                ])
                             else: # mania
-                                if bmap.mode.as_vanilla != 3:
-                                    resp_msg = 'Mania converts not currently supported.'
+                                if r_match['mods'] is not None:
+                                    # [1:] to remove leading whitespace
+                                    mods_str = r_match['mods'][1:]
+                                    mods = Mods.from_np(mods_str, mode_vn)
                                 else:
-                                    if r_match['mods'] is not None:
-                                        # [1:] to remove leading whitespace
-                                        mods_str = r_match['mods'][1:]
-                                        mods = int(Mods.from_np(mods_str, mode_vn))
-                                    else:
-                                        mods = 0
+                                    mods = None
 
-                                    calc = Maniera(str(osu_file_path), mods, 0)
-                                    calc.sr = calc._calculateStars()
-                                    pp_values = []
+                                beatmap = PeaceMap(str(osu_file_path))
+                                peace = Calculator()
 
-                                    for score in glob.config.pp_cached_scores:
-                                        calc.score = score
+                                if mods is not None:
+                                    peace.set_mods(int(mods))
 
-                                        pp = calc._calculatePP()
+                                peace.set_mode(mode_vn)
 
-                                        pp_values.append((score, pp))
+                                pp_values = []
 
-                                    resp_msg = ' | '.join([
-                                        f'{int(score // 1000)}k: {pp:,.2f}pp'
-                                        for score, pp in pp_values
-                                    ])
+                                for score in glob.config.pp_cached_scores:
+                                    peace.set_score(int(score))
+
+                                    calc = peace.calculate(beatmap)
+
+                                    pp_values.append((score, calc.pp))
+
+                                resp_msg = ' | '.join([
+                                    f'{int(score // 1000)}k: {pp:,.2f}pp'
+                                    for score, pp in pp_values
+                                ])
 
                             elapsed = time.time_ns() - pp_calc_st
                             resp_msg += f' | Elapsed: {magnitude_fmt_time(elapsed)}'
--- objects/score.py
+++ objects/score.py
@@ -13,7 +13,8 @@ from typing import TYPE_CHECKING
 from cmyui.logging import Ansi
 from cmyui.logging import log
 from cmyui.osu.oppai_ng import OppaiWrapper
-from maniera.calculator import Maniera
+from peace_performance_python.objects import Beatmap as PeaceMap
+from peace_performance_python.objects import Calculator
 from py3rijndael import Pkcs7Padding
 from py3rijndael import RijndaelCbc
 
@@ -338,7 +339,7 @@ class Score:
         """Calculate PP and star rating for our score."""
         mode_vn = self.mode.as_vanilla
 
-        if mode_vn in (0, 1): # osu, taiko
+        if mode_vn == 0: # std
             with OppaiWrapper('oppai-ng/liboppai.so') as ezpp:
                 if self.mods:
                     ezpp.set_mods(int(self.mods))
@@ -357,21 +358,48 @@ class Score:
                     return (pp, ezpp.get_sr())
                 else:
                     return (0.0, 0.0)
-        elif mode_vn == 2: # catch
-            return (0.0, 0.0)
-        else: # mania
-            if self.bmap.mode.as_vanilla != 3:
-                return (0.0, 0.0) # maniera has no convert support
+        elif mode_vn in (1, 2): # taiko, catch
+            beatmap = PeaceMap(osu_file_path)
+            peace = Calculator()
 
             if self.mods != Mods.NOMOD:
-                mods = int(self.mods)
+                peace.set_mods(int(self.mods))
+
+            if mode_vn:
+                peace.set_mode(mode_vn)
+
+            peace.set_combo(self.max_combo)
+            peace.set_miss(self.nmiss)
+            peace.set_acc(self.acc)
+
+            calculated = peace.calculate(beatmap)
+            
+            if calculated.pp not in (math.inf, math.nan):
+                temp_pp = round(calculated.pp, 5)
+
+                if (mode_vn == 1 and beatmap.diff > 0 and temp_pp > 800) or calculated.stars > 50:
+                    return (0.0, 0.0)
+                else:
+                    return (temp_pp, calculated.stars)
             else:
-                mods = 0
+                return (0.0, 0.0)
+        elif mode_vn == 3: # mania
+            beatmap = PeaceMap(osu_file_path)
+            peace = Calculator()
 
-            calc = Maniera(str(osu_file_path), mods, self.score)
-            calc.calculate()
+            if self.mods != Mods.NOMOD:
+                peace.set_mods(int(self.mods))
 
-            return (calc.pp, calc.sr)
+            if mode_vn:
+                peace.set_mode(mode_vn)
+
+            peace.set_score(int(self.score))
+            calculated = peace.calculate(beatmap)
+
+            if calculated.pp not in (math.inf, math.nan):
+                return (round(calculated.pp, 5), calculated.stars)
+            else:
+                return (0.0, 0.0)
 
     async def calc_status(self) -> None:
         """Calculate the submission status of a submitted score."""
