11918bf58acf1df593eff52ec1e75c0509a9ecce
Myroslav Sukhoy <belofost22@gmail.com>
2021-09-10 20:47:16 +0300

    Updated !with to work with peace-performance

--- constants/commands.py
+++ constants/commands.py
@@ -3,6 +3,7 @@
 import asyncio
 import copy
 import importlib
+import math
 import os
 import pprint
 import random
@@ -23,6 +24,8 @@ from typing import Sequence
 from typing import TYPE_CHECKING
 from typing import Union
 from pathlib import Path
+from peace_performance_python.objects import Beatmap as peaceMap
+from peace_performance_python.objects import Calculator
 
 import aiomysql
 import cmyui.utils
@@ -334,7 +337,7 @@ async def _with(ctx: Context) -> str:
 
     mode_vn = ctx.player.last_np['mode_vn']
 
-    if mode_vn in (0, 1): # osu, taiko
+    if mode_vn in (0, 1, 2): # osu, taiko, catch
         if not ctx.args or len(ctx.args) > 4:
             return 'Invalid syntax: !with <acc/nmiss/combo/mods ...>'
 
@@ -384,29 +387,62 @@ async def _with(ctx: Context) -> str:
 
         msg = []
 
-        with OppaiWrapper('oppai-ng/liboppai.so') as ezpp:
+        if mode_vn == 1:
+            with OppaiWrapper('oppai-ng/liboppai.so') as ezpp:
+                if mods is not None:
+                    ezpp.set_mods(int(mods))
+                    msg.append(f'{mods!r}')
+
+                if nmiss is not None:
+                    ezpp.set_nmiss(nmiss)
+                    msg.append(f'{nmiss}m')
+
+                if combo is not None:
+                    ezpp.set_combo(combo)
+                    msg.append(f'{combo}x')
+
+                if acc is not None:
+                    ezpp.set_accuracy_percent(acc)
+                    msg.append(f'{acc:.2f}%')
+
+                ezpp.calculate(osu_file_path)
+                pp, sr = ezpp.get_pp(), ezpp.get_sr()
+
+                return f"{' '.join(msg)}: {pp:.2f}pp ({sr:.2f}*)"
+        else:
+            beatmap = peaceMap(str(osu_file_path))
+            peace = Calculator()
+
             if mods is not None:
-                ezpp.set_mods(int(mods))
+                peace.set_mods(int(mods))
                 msg.append(f'{mods!r}')
 
             if nmiss is not None:
-                ezpp.set_nmiss(nmiss)
+                peace.set_miss(nmiss)
                 msg.append(f'{nmiss}m')
 
             if combo is not None:
-                ezpp.set_combo(combo)
+                peace.set_combo(combo)
                 msg.append(f'{combo}x')
-
+            
             if acc is not None:
-                ezpp.set_accuracy_percent(acc)
+                peace.set_acc(acc)
                 msg.append(f'{acc:.2f}%')
 
-            ezpp.calculate(osu_file_path)
-            pp, sr = ezpp.get_pp(), ezpp.get_sr()
+            if mode_vn:
+                peace.set_mode(mode_vn)
 
-            return f"{' '.join(msg)}: {pp:.2f}pp ({sr:.2f}*)"
-    elif mode_vn == 2: # catch
-        return 'Gamemode not yet supported.'
+            calculated = peace.calculate(beatmap)
+            
+            if calculated.pp not in (math.inf, math.nan):
+                temp_pp = round(calculated.pp, 5)
+                # Some of taiko converted maps are failed to calculate, return zero
+                if (mode_vn == 1 and beatmap.diff > 0 and temp_pp > 800) or calculated.stars > 50:
+                    return f"{' '.join(msg)}: 0pp (0*)" 
+                else:
+                    return f"{' '.join(msg)}: {temp_pp:.2f}pp ({calculated.stars:.2f}*)"
+            else:
+                return f"{' '.join(msg)}: 0pp (0*)"
     else: # mania
         if not ctx.args or len(ctx.args) > 2:
             return 'Invalid syntax: !with <score/mods ...>'
@@ -425,11 +461,20 @@ async def _with(ctx: Context) -> str:
                 mods = mods.filter_invalid_combos(mode_vn)
             else:
                 return 'Invalid syntax: !with <score/mods ...>'
+        
+        beatmap = peaceMap(str(osu_file_path))
+        peace = Calculator()
 
-        calc = Maniera(str(osu_file_path), int(mods), score * 1000)
-        calc.calculate()
+        if mods != Mods.NOMOD:
+            peace.set_mods(int(mods))
+        
+        if mode_vn:
+            peace.set_mode(mode_vn)
 
-        return f'{score}k {mods!r}: {calc.pp:.2f}pp ({calc.sr:.2f}*)'
+        peace.set_score(int(score * 1000))
+        
+        calc = peace.calculate(beatmap)
+        return f'{score}k {mods!r}: {calc.pp:.2f}pp ({calc.stars:.2f}*)'
 
 @command(Privileges.Normal, aliases=['req'])
 async def request(ctx: Context) -> str:
