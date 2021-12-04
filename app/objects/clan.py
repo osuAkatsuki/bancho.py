af9de6cbe15026e9c4d7323c75e949a0fb21e5ea
tsunyoku <tsunyoku>
2021-12-04 21:10:36 +0000

    port clan object to sqlalchemy

--- app/objects/clan.py
+++ app/objects/clan.py
@@ -4,9 +4,13 @@ from enum import unique
 from typing import TYPE_CHECKING
 
 import aiomysql
+from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from app.misc.utils import escape_enum
 from app.misc.utils import pymysql_encode
+
+import app.services
+import app.db_models
 
 if TYPE_CHECKING:
     from app.objects.player import Player
@@ -51,10 +55,15 @@ class Clan:
         """Add a given player to the clan's members."""
         self.members.add(p.id)
 
-        await services.database.execute(
-            "UPDATE users SET clan_id = %s, clan_priv = 1 WHERE id = %s",
-            [self.id, p.id],
-        )
+        async with app.services.database_session() as db_conn:
+            await db_conn.execute(
+                app.db_models.users.update(
+                    values={
+                        "clan_id": self.id,
+                        "clan_priv": 1,
+                    }
+                ).where(app.db_models.users.c.id == p.id)
+            )
 
         p.clan = self
         p.clan_priv = ClanPrivileges.Member
@@ -63,47 +72,62 @@ class Clan:
         """Remove a given player from the clan's members."""
         self.members.remove(p.id)
 
-        async with services.database.connection() as conn:
-            async with conn.cursor() as db_cursor:
-                await db_cursor.execute(
-                    "UPDATE users SET clan_id = 0, clan_priv = 0 WHERE id = %s",
-                    [p.id],
+        async with app.services.database_session() as db_conn:
+            await db_conn.execute(
+                app.db_models.users.update(
+                    values={
+                        "clan_id": 0,
+                        "clan_priv": 0,
+                    }
+                ).where(app.db_models.users.c.id == p.id)
+            )
+
+            if not self.members:
+                # no members left, disband clan.
+                await db_conn.execute(
+                    app.db_models.clans.delete().where(
+                        app.db_models.clans.c.id == self.id
+                    )
+                )
+            elif p.id == self.owner:
+                # owner leaving and members left,
+                # transfer the ownership.
+                # TODO: prefer officers
+                self.owner = next(iter(self.members))
+
+                await db_conn.execute(
+                    app.db_models.clans.update(
+                        values={
+                            "owner": self.owner,
+                        }
+                    ).where(app.db_models.clans.c.id == self.id)
                 )
 
-                if not self.members:
-                    # no members left, disband clan.
-                    await db_cursor.execute(
-                        "DELETE FROM clans WHERE id = %s",
-                        [self.id],
-                    )
-                elif p.id == self.owner:
-                    # owner leaving and members left,
-                    # transfer the ownership.
-                    # TODO: prefer officers
-                    self.owner = next(iter(self.members))
-
-                    await db_cursor.execute(
-                        "UPDATE clans SET owner = %s WHERE id = %s",
-                        [self.owner, self.id],
-                    )
-
-                    await db_cursor.execute(
-                        "UPDATE users SET clan_priv = 3 WHERE id = %s",
-                        [self.owner],
-                    )
+                await db_conn.execute(
+                    app.db_models.users.update(
+                        values={
+                            "clan_priv": 3,
+                        }
+                    ).where(app.db_models.users.c.id == self.owner)
+                )
 
         p.clan = None
         p.clan_priv = None
 
-    async def members_from_sql(self, db_cursor: aiomysql.DictCursor) -> None:
+    async def members_from_sql(self, db_conn: AsyncSession) -> None:
         """Fetch all members from sql."""
         # TODO: in the future, we'll want to add
         # clan 'mods', so fetching rank here may
         # be a good idea to sort people into
         # different roles.
-        await db_cursor.execute("SELECT id FROM users WHERE clan_id = %s", [self.id])
 
-        async for row in db_cursor:
+        user_res = await db_conn.execute(
+            app.db_models.users.select(app.db_models.users.c.id).where(
+                app.db_models.users.c.clan_id == self.id
+            )
+        )
+
+        for row in user_res.fetchall():
             self.members.add(row["id"])
 
     def __repr__(self) -> str:
