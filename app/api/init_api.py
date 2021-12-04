94d93f620b4a51d56729e211d301124f0934524a
cmyui <cmyuiosu@gmail.com>
2021-12-04 05:13:08 -0500

    clean up api app initialization

--- app/api/init_api.py
+++ app/api/init_api.py
@@ -1,284 +1,92 @@
 # #!/usr/bin/env python3.10
-import os
+"""
+gulag - the most developed, production-ready osu! server implementation
 
+if you're interested in development, my test server is usually
+up at https://c.cmyui.xyz. just use the same `-devserver cmyui.xyz`
+connection method you would with any other modern server and you
+should have no problems connecting. registration is done in-game
+with osu!'s built-in registration (if you're worried about not being
+properly connected while registering, the server should send back
+https://i.cmyui.xyz/8-Vzy9NllPBp5K7L.png if you use a random login).
+
+you can also test gulag's rest api using my test server,
+e.g https://osu.cmyui.xyz/api/get_player_scores?id=3&scope=best
+"""
+import os
+from pathlib import Path
+
+import cmyui
 from cmyui.logging import Ansi
 from cmyui.logging import log
 from fastapi import FastAPI
 
 from .domains import ava
 from .domains import cho
-from .domains import map
 from .domains import osu
+from app import services
+from app import settings
 from app.objects import glob
 
+# from .domains import map
 
-def init_domains(app: FastAPI) -> None:
-    for domain in ("ppy.sh", glob.config.domain):
+GEOLOC_DB_FILE = Path.cwd() / "ext/GeoLite2-City.mmdb"
+
+# # !! review code that uses this before modifying it.
+glob.version = cmyui.Version(3, 6, 1)
+
+
+# TODO:
+# - redis
+# - http session
+# - datadog
+# - maxmind db
+# - dependency management
+# - database migrations
+# - housekeeping tasks
+# - initialize ram caches
+# - safety checks
+# - offline mode
+# - install debugging hooks
+# - uvloop
+
+
+def init_events(app: FastAPI) -> None:
+    """Initialize our app's event handlers."""
+
+    @app.on_event("startup")
+    async def on_startup() -> None:
+        if os.geteuid() == 0:
+            log(
+                "Running the server with root privileges is not recommended.",
+                Ansi.LRED,
+            )
+
+        await services.database.connect()
+
+    @app.on_event("shutdown")
+    async def on_shutdown() -> None:
+        await services.database.disconnect()
+
+
+def init_routes(app: FastAPI) -> None:
+    """Initialize our app's route endpoints."""
+    for domain in ("ppy.sh", settings.DOMAIN):
         app.host(f"a.{domain}", ava.router)
+        app.host(f"osu.{domain}", osu.router)
 
         for subdomain in ("c", "ce", "c4", "c5", "c6"):
             app.host(f"{subdomain}.{domain}", cho.router)
 
 
 def init_api() -> FastAPI:
+    """Create & initialize our app."""
     app = FastAPI()
 
-    init_domains(app)
+    init_events(app)
+    init_routes(app)
 
     return app
 
 
 app = init_api()
-
-# """
-# gulag - an awesome osu! server implementation from TCP/IP sockets.
-
-# if you're interested in development, my test server is usually
-# up at https://c.cmyui.xyz. just use the same `-devserver cmyui.xyz`
-# connection method you would with any other modern server and you
-# should have no problems connecting. registration is done in-game
-# with osu!'s built-in registration (if you're worried about not being
-# properly connected while registering, the server should send back
-# https://i.cmyui.xyz/8-Vzy9NllPBp5K7L.png if you use a random login).
-
-# you can also test gulag's rest api using my test server,
-# e.g https://osu.cmyui.xyz/api/get_player_scores?id=3&scope=best
-# """
-# import asyncio
-# import os
-# import signal
-# import socket
-# from datetime import datetime
-# from pathlib import Path
-
-# import aiomysql
-# import cmyui
-# from cmyui.logging import log
-# from cmyui.logging import RGB
-
-# import bg_loops
-# import app.misc.context
-# import app.misc.utils
-# import objects.collections
-
-# # set the current working directory to /gulag
-# os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-# if not os.path.exists("config.py"):
-#     app.misc.utils.create_config_from_default()
-#     raise SystemExit(1)
-
-# from app.objects import glob  # (includes config)
-import cmyui
-
-# # !! review code that uses this before modifying it.
-glob.version = cmyui.Version(3, 6, 1)
-
-# GEOLOC_DB_FILE = Path.cwd() / "ext/GeoLite2-City.mmdb"
-
-
-# async def run_server() -> None:
-#     """Begin listening for and handling connections on all endpoints."""
-
-#     # we'll be working on top of transport layer posix sockets.
-#     # these implement tcp/ip over ethernet for us, and osu!stable
-#     # uses http/1.0 ontop of this. we'll need to parse the http data,
-#     # find the appropriate handler, and dispatch the connection.
-
-#     # i'll be using my light web framework to handle parsing & dispatching
-#     # of connections to their respective handlers; here, we'll just worry
-#     # about the socket-level details, like receiving the data from the clients.
-
-#     # if you're interested in more details, you can see the implementation at
-#     # https://github.com/cmyui/cmyui_pkg/blob/master/cmyui/web.py
-
-#     # fetch our server's endpoints; gulag supports
-#     # osu!'s handlers across multiple domains.
-#     from domains.cho import domain as c_ppy_sh  # /c[e4-6]?.ppy.sh/
-#     from domains.osu import domain as osu_ppy_sh
-#     from domains.ava import domain as a_ppy_sh
-#     from domains.map import domain as b_ppy_sh
-
-#     glob.app.add_domains({c_ppy_sh, osu_ppy_sh, a_ppy_sh, b_ppy_sh})
-
-#     # support both INET and UNIX sockets
-#     if app.misc.utils.is_inet_address(glob.config.server_addr):
-#         sock_family = socket.AF_INET
-#     elif isinstance(glob.config.server_addr, str):
-#         sock_family = socket.AF_UNIX
-#     else:
-#         raise ValueError("Invalid socket address.")
-
-#     if sock_family == socket.AF_UNIX:
-#         # using unix socket - remove from filesystem if it exists
-#         if os.path.exists(glob.config.server_addr):
-#             os.remove(glob.config.server_addr)
-
-#     # create our transport layer socket; osu! uses tcp/ip
-#     with socket.socket(sock_family, socket.SOCK_STREAM) as listening_sock:
-#         listening_sock.setblocking(False)  # asynchronous
-#         listening_sock.bind(glob.config.server_addr)
-
-#         if sock_family == socket.AF_UNIX:
-#             # using unix socket - give the socket file
-#             # appropriate (read, write) permissions
-#             os.chmod(glob.config.server_addr, 0o666)
-
-#         listening_sock.listen(glob.config.max_conns)
-#         log(f"-> Listening @ {glob.config.server_addr}", RGB(0x00FF7F))
-
-#         glob.ongoing_conns = []
-#         glob.shutting_down = False
-
-#         while not glob.shutting_down:
-#             # TODO: this timeout based-solution can be heavily
-#             #       improved and refactored out.
-#             try:
-#                 conn, _ = await asyncio.wait_for(
-#                     fut=glob.loop.sock_accept(listening_sock),
-#                     timeout=0.25,
-#                 )
-#             except asyncio.TimeoutError:
-#                 pass
-#             else:
-#                 task = glob.loop.create_task(glob.app.handle(conn))
-#                 task.add_done_callback(misc.utils._conn_finished_cb)
-#                 glob.ongoing_conns.append(task)
-
-#     if sock_family == socket.AF_UNIX:
-#         # using unix socket - remove from filesystem
-#         os.remove(glob.config.server_addr)
-
-
-# async def main() -> int:
-#     """Initialize, and start up the server."""
-#     glob.loop = asyncio.get_running_loop()
-
-#     async with (
-#         misc.context.acquire_http_session(glob.has_internet) as glob.http_session,
-#         misc.context.acquire_mysql_db_pool(glob.config.mysql) as services.database,
-#         misc.context.acquire_redis_db_pool() as glob.redis,
-#     ):
-#         await app.misc.utils.check_for_dependency_updates()
-#         await app.misc.utils.run_sql_migrations()
-
-#         with (
-#             misc.context.acquire_geoloc_db_conn(GEOLOC_DB_FILE) as glob.geoloc_db,
-#             misc.context.acquire_datadog_client(glob.config.datadog) as glob.datadog,
-#         ):
-#             # TODO: refactor debugging so
-#             # this can be moved to `run_server`.
-#             glob.app = cmyui.Server(
-#                 name=f"gulag v{glob.version}",
-#                 gzip=4,
-#                 debug=glob.config.debug,
-#             )
-
-#             # prepare our ram caches, populating from sql where necessary.
-#             # this includes channels, clans, mappools, bot info, etc.
-#             async with services.database.connection() as conn:
-#                 async with conn.cursor(aiomysql.DictCursor) as db_cursor:
-#                     await objects.collections.initialize_ram_caches(db_cursor)  # type: ignore
-
-#             # initialize housekeeping tasks to automatically manage
-#             # and ensure memory on ram & disk are kept up to date.
-#             await bg_loops.initialize_housekeeping_tasks()
-
-#             # handle signals so we can ensure a graceful shutdown
-#             sig_handler = app.misc.utils.shutdown_signal_handler
-#             for signum in (signal.SIGINT, signal.SIGTERM, signal.SIGHUP):
-#                 glob.loop.add_signal_handler(signum, sig_handler, signum)
-
-#             # TODO: restart signal handler with SIGUSR1
-
-#             # run the server, handling connections
-#             # until a termination signal is received.
-#             await run_server()
-
-#             # we want to attempt to gracefully finish any ongoing connections
-#             # and shut down any of the housekeeping tasks running in the background.
-
-#             if glob.ongoing_conns:
-#                 await app.misc.utils.await_ongoing_connections(timeout=5.0)
-
-#             await app.misc.utils.cancel_housekeeping_tasks()
-
-#     return 0
-
-
-# if __name__ == "__main__":
-#     """After basic safety checks, start the event loop and call our async entry point."""
-#     app.misc.utils.setup_runtime_environment()
-
-#     for safety_check in (
-#         app.misc.utils.ensure_supported_platform,  # linux only at the moment
-#         app.misc.utils.ensure_local_services_are_running,  # mysql (if local)
-#         app.misc.utils.ensure_directory_structure,  # .data/ & achievements/ dir structure
-#         app.misc.utils.ensure_dependencies_and_requirements,  # submodules & oppai-ng built
-#     ):
-#         if (exit_code := safety_check()) != 0:
-#             raise SystemExit(exit_code)
-
-#     """ Server should be safe to start """
-
-#     glob.boot_time = datetime.now()
-
-#     # install any debugging hooks from
-#     # _testing/runtime.py, if present
-#     app.misc.utils._install_debugging_hooks()
-
-#     # check our internet connection status
-#     glob.has_internet = app.misc.utils.check_connection(timeout=1.5)
-
-#     # show info & any contextual warnings.
-#     app.misc.utils.display_startup_dialog()
-
-#     try:
-#         # use uvloop if available
-#         # https://github.com/MagicStack/uvloop
-#         import uvloop
-
-#         uvloop.install()
-#     except ModuleNotFoundError:
-#         pass
-
-#     raise SystemExit(asyncio.run(main()))
-
-# elif __name__ == "main":
-#     # check specifically for ASGI servers; many related projects use
-#     # them to run in production, and devs may assume we do as well.
-#     if app.misc.utils.running_via_asgi_webserver():
-#         raise RuntimeError(
-#             "gulag implements it's own web framework implementation from "
-#             "transport layer (tcp/ip) posix sockets and does not rely on "
-#             "an ASGI server to serve connections; run it directy, `./main.py`",
-#         )
-#     else:
-#         raise RuntimeError("gulag should only be run directly, `./main.py`")
-
-
-def display_startup_dialog() -> None:
-    """Print any general information or warnings to the console."""
-    if glob.config.advanced:
-        log("running in advanced mode", Ansi.LRED)
-
-    # running on root grants the software potentally dangerous and
-    # unnecessary power over the operating system and is not advised.
-    if os.geteuid() == 0:
-        log(
-            "It is not recommended to run gulag as root, especially in production..",
-            Ansi.LYELLOW,
-        )
-
-        if glob.config.advanced:
-            log(
-                "The risk is even greater with features "
-                "such as config.advanced enabled.",
-                Ansi.LRED,
-            )
-
-    if not glob.has_internet:
-        log(
-            "Running in offline mode, some features will not be available.",
-            Ansi.LRED,
-        )
