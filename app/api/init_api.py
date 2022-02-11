ace0847eeb38938a6ae5a4ae353e3bd5598f2d95
cmyui <cmyuiosu@gmail.com>
2022-02-11 17:47:32 -0500

    re-add validation error exception handler

--- app/api/init_api.py
+++ app/api/init_api.py
@@ -1,13 +1,18 @@
 # #!/usr/bin/env python3.9
 import asyncio
 import os
+import pprint
 
 import aiohttp
 import orjson
 from cmyui.logging import Ansi
 from cmyui.logging import log
 from fastapi import FastAPI
+from fastapi import status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from fastapi.requests import Request
+from fastapi.responses import ORJSONResponse
 from fastapi.responses import Response
 from starlette.middleware.base import RequestResponseEndpoint
 
@@ -18,6 +23,22 @@ import settings
 from app.api import domains
 from app.api import middlewares
 from app.objects import collections
+
+
+def init_exception_handlers(asgi_app: FastAPI) -> None:
+    @asgi_app.exception_handler(RequestValidationError)
+    async def handle_validation_error(
+        request: Request,
+        exc: RequestValidationError,
+    ) -> Response:
+        """Wrapper around 422 validation errors to print out info for devs."""
+        log(f"Validation error on {request.url}", Ansi.LRED)
+        pprint.pprint(exc.errors())
+
+        return ORJSONResponse(
+            content={"detail": jsonable_encoder(exc.errors())},
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        )
 
 
 def init_middlewares(asgi_app: FastAPI) -> None:
@@ -120,6 +141,7 @@ def init_api() -> FastAPI:
     asgi_app = FastAPI()
 
     init_middlewares(asgi_app)
+    init_exception_handlers(asgi_app)
     init_events(asgi_app)
     init_routes(asgi_app)
 
