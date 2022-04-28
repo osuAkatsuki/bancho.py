6183fe68f5f29191ec3d7c12d41434d100855f20
Aochi <aochi@fuquila.net>
2022-04-28 22:50:36 +0200

    forgot

--- app/commands.py
+++ app/commands.py
@@ -904,6 +904,7 @@ async def user(ctx: Context) -> Optional[str]:
         (
             f'[{"Bot" if p.bot_client else "Player"}] {p.full_name} ({p.id})',
             f"Privileges: {priv_list}",
+            f"Donator ends: {timeago.format(p.donor_end)}",
             f"Channels: {[p._name for p in p.channels]}",
             f"Logged in: {timeago.format(p.login_time)}",
             f"Last server interaction: {timeago.format(p.last_recv_time)}",
