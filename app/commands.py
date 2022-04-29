88ebaa78a1768b5d81af8c312472d0f1b1fb8c67
minisbett <39670899+minisbett@users.noreply.github.com>
2022-04-29 19:57:38 +0200

    cmyui do be nitpicking

--- app/commands.py
+++ app/commands.py
@@ -1407,13 +1407,14 @@ async def rmpriv(ctx: Context) -> Optional[str]:
     if not (t := await app.state.sessions.players.from_cache_or_sql(name=ctx.args[0])):
         return "Could not find user."
 
+    await t.remove_privs(bits)
+   
     if bits & Privileges.DONATOR:
         await app.state.services.database.execute(
             "UPDATE users SET donor_end = 0 WHERE id = :user_id",
             {"user_id": t.id},
         )
-
-    await t.remove_privs(bits)
+        
     return f"Updated {t}'s privileges."
 
 
