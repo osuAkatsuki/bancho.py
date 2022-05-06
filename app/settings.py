817e3b34601bba5b7bd9d8766fa94068bfe446a1
cmyui <cmyuiosu@gmail.com>
2022-05-06 19:16:11 -0400

    remove unnecessary defaults from settings (py layer)

--- app/settings.py
+++ app/settings.py
@@ -21,33 +21,20 @@ REDIS_DSN: str = config("REDIS_DSN")
 
 OSU_API_KEY: Secret = config("OSU_API_KEY", cast=Secret)
 
-DOMAIN: str = config("DOMAIN", default="cmyui.xyz")
-MIRROR_URL: str = config("MIRROR_URL", default="https://api.chimu.moe/v1")
+DOMAIN: str = config("DOMAIN")
+MIRROR_URL: str = config("MIRROR_URL")
 
-COMMAND_PREFIX: str = config("COMMAND_PREFIX", default="!")
+COMMAND_PREFIX: str = config("COMMAND_PREFIX")
+SEASONAL_BGS: CommaSeparatedStrings = config("SEASONAL_BGS", cast=CommaSeparatedStrings)
 
-SEASONAL_BGS: CommaSeparatedStrings = config(
-    "SEASONAL_BGS",
-    cast=CommaSeparatedStrings,
-    default=CommaSeparatedStrings(
-        [
-            "https://akatsuki.pw/static/flower.png",
-            "https://i.cmyui.xyz/nrMT4V2RR3PR.jpeg",
-        ],
-    ),
-)
-
-MENU_ICON_URL: str = config(
-    "MENU_ICON_URL",
-    default="https://akatsuki.pw/static/logos/logo_ingame.png",
-)
-MENU_ONCLICK_URL: str = config("MENU_ONCLICK_URL", default="https://akatsuki.pw")
+MENU_ICON_URL: str = config("MENU_ICON_URL")
+MENU_ONCLICK_URL: str = config("MENU_ONCLICK_URL")
 
 DATADOG_API_KEY: Secret = config("DATADOG_API_KEY", cast=Secret)
 DATADOG_APP_KEY: Secret = config("DATADOG_APP_KEY", cast=Secret)
 
-DEBUG: bool = config("DEBUG", cast=bool, default=False)
-REDIRECT_OSU_URLS: bool = config("REDIRECT_OSU_URLS", cast=bool, default=True)
+DEBUG: bool = config("DEBUG", cast=bool)
+REDIRECT_OSU_URLS: bool = config("REDIRECT_OSU_URLS", cast=bool)
 
 PP_CACHED_ACCURACIES: list[int] = [
     int(acc)
@@ -74,7 +61,6 @@ DISCORD_AUDIT_LOG_WEBHOOK: str = config("DISCORD_AUDIT_LOG_WEBHOOK")
 AUTOMATICALLY_REPORT_PROBLEMS: bool = config(
     "AUTOMATICALLY_REPORT_PROBLEMS",
     cast=bool,
-    default=True,
 )
 
 # advanced dev settings
@@ -82,7 +68,7 @@ AUTOMATICALLY_REPORT_PROBLEMS: bool = config(
 ## WARNING: only touch this once you've
 ##          read through what it enables.
 ##          you could put your server at risk.
-DEVELOPER_MODE: bool = config("DEVELOPER_MODE", cast=bool, default=False)
+DEVELOPER_MODE: bool = config("DEVELOPER_MODE", cast=bool)
 
 ## WARNING: only touch this if you know how
 ##          the migrations system works.
