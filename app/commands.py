a0de9c731726e35bb91c91239c424701e2e35787
minisbett <39670899+minisbett@users.noreply.github.com>
2022-05-07 00:40:21 +0200

    optimization for donator info in !user command (#234)
    * uhhh
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- app/commands.py
+++ app/commands.py
@@ -896,7 +896,9 @@ async def user(ctx: Context) -> Optional[str]:
 
     osu_version = p.client_details.osu_version.date if p.online else "Unknown"
     donator_info = (
-        f"until {timeago.format(p.donor_end)}" if p.donor_end > time.time() else "no"
+        f"True (ends {timeago.format(p.donor_end)})"
+        if p.priv & Privileges.DONATOR
+        else "False"
     )
 
     return "\n".join(
