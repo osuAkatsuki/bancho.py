add37cc0fbf7f6dc283ffd5766865b3483adf7fa
minisbett <39670899+minisbett@users.noreply.github.com>
2022-05-07 10:08:30 +0200

    Fixed code put in wrong command

--- app/commands.py
+++ app/commands.py
@@ -1385,6 +1385,9 @@ async def addpriv(ctx: Context) -> Optional[str]:
 
     if not (t := await app.state.sessions.players.from_cache_or_sql(name=ctx.args[0])):
         return "Could not find user."
+    
+    if bits & Privileges.DONATOR:
+        return "Please use the !givedonator command to assign donator privileges to players."
 
     await t.add_privs(bits)
     return f"Updated {t}'s privileges."
@@ -1406,9 +1409,6 @@ async def rmpriv(ctx: Context) -> Optional[str]:
 
     if not (t := await app.state.sessions.players.from_cache_or_sql(name=ctx.args[0])):
         return "Could not find user."
-
-    if bits & Privileges.DONATOR:
-        return "Please use the !givedonator command to assign donator privileges to players."
 
     await t.remove_privs(bits)
     return f"Updated {t}'s privileges."
