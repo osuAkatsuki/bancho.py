7759ee01a729b38905cc3443deb9e3ba7a8d690f
cmyui <cmyuiosu@gmail.com>
2022-07-18 01:35:31 -0400

    remove (pointless) type annotations from settings.py

--- app/settings.py
+++ app/settings.py
@@ -9,22 +9,20 @@ from starlette.datastructures import Secret
 
 config = Config(".env")
 
-SERVER_ADDR: str = config("SERVER_ADDR")
-SERVER_PORT: Optional[int] = (
-    int(v) if (v := config("SERVER_PORT", default=None)) else None
-)
+SERVER_ADDR = config("SERVER_ADDR")
+SERVER_PORT = int(v) if (v := config("SERVER_PORT", default=None)) else None
 
-DB_DSN: DatabaseURL = config("DB_DSN", cast=DatabaseURL)
-REDIS_DSN: str = config("REDIS_DSN")
+DB_DSN = config("DB_DSN", cast=DatabaseURL)
+REDIS_DSN = config("REDIS_DSN")
 
-OSU_API_KEY: Secret = config("OSU_API_KEY", cast=Secret)
+OSU_API_KEY = config("OSU_API_KEY", cast=Secret)
 
-DOMAIN: str = config("DOMAIN", default="cmyui.xyz")
-MIRROR_URL: str = config("MIRROR_URL", default="https://api.chimu.moe/v1")
+DOMAIN = config("DOMAIN", default="cmyui.xyz")
+MIRROR_URL = config("MIRROR_URL", default="https://api.chimu.moe/v1")
 
-COMMAND_PREFIX: str = config("COMMAND_PREFIX", default="!")
+COMMAND_PREFIX = config("COMMAND_PREFIX", default="!")
 
-SEASONAL_BGS: CommaSeparatedStrings = config(
+SEASONAL_BGS = config(
     "SEASONAL_BGS",
     cast=CommaSeparatedStrings,
     default=CommaSeparatedStrings(
@@ -35,26 +33,26 @@ SEASONAL_BGS: CommaSeparatedStrings = config(
     ),
 )
 
-MENU_ICON_URL: str = config(
+MENU_ICON_URL = config(
     "MENU_ICON_URL",
     default="https://akatsuki.pw/static/logos/logo_ingame.png",
 )
-MENU_ONCLICK_URL: str = config("MENU_ONCLICK_URL", default="https://akatsuki.pw")
+MENU_ONCLICK_URL = config("MENU_ONCLICK_URL", default="https://akatsuki.pw")
 
-DATADOG_API_KEY: Secret = config("DATADOG_API_KEY", cast=Secret)
-DATADOG_APP_KEY: Secret = config("DATADOG_APP_KEY", cast=Secret)
+DATADOG_API_KEY = config("DATADOG_API_KEY", cast=Secret)
+DATADOG_APP_KEY = config("DATADOG_APP_KEY", cast=Secret)
 
-DEBUG: bool = config("DEBUG", cast=bool, default=False)
-REDIRECT_OSU_URLS: bool = config("REDIRECT_OSU_URLS", cast=bool, default=True)
+DEBUG = config("DEBUG", cast=bool, default=False)
+REDIRECT_OSU_URLS = config("REDIRECT_OSU_URLS", cast=bool, default=True)
 
-PP_CACHED_ACCURACIES: list[int] = [
+PP_CACHED_ACCURACIES = [
     int(acc)
     for acc in config(
         "PP_CACHED_ACCS",
         cast=CommaSeparatedStrings,
     )
 ]
-PP_CACHED_SCORES: list[int] = [
+PP_CACHED_SCORES = [
     int(score)
     for score in config(
         "PP_CACHED_SCORES",
@@ -62,18 +60,18 @@ PP_CACHED_SCORES: list[int] = [
     )
 ]
 
-DISALLOWED_NAMES: CommaSeparatedStrings = config(
+DISALLOWED_NAMES = config(
     "DISALLOWED_NAMES",
     cast=CommaSeparatedStrings,
 )
-DISALLOWED_PASSWORDS: CommaSeparatedStrings = config(
+DISALLOWED_PASSWORDS = config(
     "DISALLOWED_PASSWORDS",
     cast=CommaSeparatedStrings,
 )
 
-DISCORD_AUDIT_LOG_WEBHOOK: str = config("DISCORD_AUDIT_LOG_WEBHOOK")
+DISCORD_AUDIT_LOG_WEBHOOK = config("DISCORD_AUDIT_LOG_WEBHOOK")
 
-AUTOMATICALLY_REPORT_PROBLEMS: bool = config(
+AUTOMATICALLY_REPORT_PROBLEMS = config(
     "AUTOMATICALLY_REPORT_PROBLEMS",
     cast=bool,
     default=True,
@@ -81,12 +79,12 @@ AUTOMATICALLY_REPORT_PROBLEMS: bool = config(
 
 # advanced dev settings
 
-## WARNING: only touch this once you've
+## WARNING touch this once you've
 ##          read through what it enables.
 ##          you could put your server at risk.
-DEVELOPER_MODE: bool = config("DEVELOPER_MODE", cast=bool, default=False)
+DEVELOPER_MODE = config("DEVELOPER_MODE", cast=bool, default=False)
 
-## WARNING: only touch this if you know how
+## WARNING touch this if you know how
 ##          the migrations system works.
 ##          you'll regret it.
 VERSION = "4.3.2"
