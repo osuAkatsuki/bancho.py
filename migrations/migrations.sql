05aa5580251f1e63f9037fbf1ba8f35a90ae15fe
cmyui <cmyuiosu@gmail.com>
2022-07-23 19:06:51 -0400

    remove mania-secret-meganekko achievement
    this can be done in a separate pr

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -388,15 +388,14 @@ alter table maps change server server enum('osu!', 'private') default 'osu!' not
 alter table mapsets change server server enum('osu!', 'private') default 'osu!' not null;
 
 # v4.4.2
-insert into achievements (id, file, name, `desc`, cond) values (73, 'mania-secret-meganekko', 'A meganekko approaches', 'Congratulations, you met Maria!', 'mode_vn == 3 and score.combo >= 100');
-insert into achievements (id, file, name, `desc`, cond) values (74, 'all-intro-suddendeath', 'Finality', 'High stakes, no regrets.', 'score.mods == 32')
-insert into achievements (id, file, name, `desc`, cond) values (75, 'all-intro-hidden', 'Blindsight', 'I can see just perfectly', 'score.mods & 8');
-insert into achievements (id, file, name, `desc`, cond) values (76, 'all-intro-perfect', 'Perfectionist', 'Accept nothing but the best.', 'score.mods & 16384');
-insert into achievements (id, file, name, `desc`, cond) values (77, 'all-intro-hardrock', 'Rock Around The Clock', "You can\'t stop the rock.", 'score.mods & 16');
-insert into achievements (id, file, name, `desc`, cond) values (78, 'all-intro-doubletime', 'Time And A Half', "Having a right ol\' time. One and a half of them, almost.", 'score.mods & 64');
-insert into achievements (id, file, name, `desc`, cond) values (79, 'all-intro-flashlight', 'Are You Afraid Of The Dark?', "Harder than it looks, probably because it\'s hard to look.", 'score.mods & 1024');
-insert into achievements (id, file, name, `desc`, cond) values (80, 'all-intro-easy', 'Dial It Right Back', 'Sometimes you just want to take it easy.', 'score.mods & 2');
-insert into achievements (id, file, name, `desc`, cond) values (81, 'all-intro-nofail', 'Risk Averse', 'Safety nets are fun!', 'score.mods & 1');
-insert into achievements (id, file, name, `desc`, cond) values (82, 'all-intro-nightcore', 'Sweet Rave Party', 'Founded in the fine tradition of changing things that were just fine as they were.', 'score.mods & 512');
+insert into achievements (id, file, name, `desc`, cond) values (73, 'all-intro-suddendeath', 'Finality', 'High stakes, no regrets.', 'score.mods == 32');
+insert into achievements (id, file, name, `desc`, cond) values (74, 'all-intro-hidden', 'Blindsight', 'I can see just perfectly', 'score.mods & 8');
+insert into achievements (id, file, name, `desc`, cond) values (75, 'all-intro-perfect', 'Perfectionist', 'Accept nothing but the best.', 'score.mods & 16384');
+insert into achievements (id, file, name, `desc`, cond) values (76, 'all-intro-hardrock', 'Rock Around The Clock', "You can\'t stop the rock.", 'score.mods & 16');
+insert into achievements (id, file, name, `desc`, cond) values (77, 'all-intro-doubletime', 'Time And A Half', "Having a right ol\' time. One and a half of them, almost.", 'score.mods & 64');
+insert into achievements (id, file, name, `desc`, cond) values (78, 'all-intro-flashlight', 'Are You Afraid Of The Dark?', "Harder than it looks, probably because it\'s hard to look.", 'score.mods & 1024');
+insert into achievements (id, file, name, `desc`, cond) values (79, 'all-intro-easy', 'Dial It Right Back', 'Sometimes you just want to take it easy.', 'score.mods & 2');
+insert into achievements (id, file, name, `desc`, cond) values (80, 'all-intro-nofail', 'Risk Averse', 'Safety nets are fun!', 'score.mods & 1');
+insert into achievements (id, file, name, `desc`, cond) values (81, 'all-intro-nightcore', 'Sweet Rave Party', 'Founded in the fine tradition of changing things that were just fine as they were.', 'score.mods & 512');
 insert into achievements (id, file, name, `desc`, cond) values (82, 'all-intro-halftime', 'Slowboat', 'You got there. Eventually.', 'score.mods & 256');
 insert into achievements (id, file, name, `desc`, cond) values (83, 'all-intro-spunout', 'Burned Out', 'One cannot always spin to win.', 'score.mods & 4096');
