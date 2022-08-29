62421ede6362a9dfb0af4b460287d457f1e92f41
cmyui <cmyuiosu@gmail.com>
2022-08-29 18:22:19 -0400

    docs: improve comment to more accurately depict change

--- app/api/domains/osu.py
+++ app/api/domains/osu.py
@@ -289,10 +289,10 @@ async def osuGetBeatmapInfo(
         # convert from bancho.py -> osu!api status
         row["status"] = bancho_to_osuapi_status(row["status"])
 
-        # try to get the user's grades on the map osu!
-        # only allows us to send back one per gamemode,
-        # so we'll just send back vanilla for the time being..
-        # XXX: perhaps user-customizable in the future?
+        # try to get the user's grades on the map
+        # NOTE: osu! only allows us to send back one per gamemode,
+        #       so we've decided to send back *vanilla* grades.
+        #       (in theory we could make this user-customizable)
         grades = ["N", "N", "N", "N"]
 
         await db_conn.execute(
