9f8a049d62442cf1cf529d900564eef370f485c8
minisbett <39670899+minisbett@users.noreply.github.com>
2022-12-31 01:35:32 +0100

    Fix incorrect migration version

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -403,7 +403,7 @@ insert into achievements (id, file, name, `desc`, cond) values (83, 'all-intro-s
 # v4.4.3
 alter table favourites add created_at int default 0 not null;
 
-# v4.4.4
+# v4.6.4
 alter table map_requests add requested_status varchar(6) default '' not null;
 alter table map_requests add comment text default '' not null;
 
