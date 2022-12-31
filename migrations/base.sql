d8ead21758cedb441f9ceb1ddf60684d5b1c2724
minisbett <39670899+minisbett@users.noreply.github.com>
2022-12-31 01:43:29 +0100

    Added requested_status and comment column to map_requests
    I forgot to do this

--- migrations/base.sql
+++ migrations/base.sql
@@ -163,6 +163,8 @@ create table map_requests
 	id int auto_increment
 		primary key,
 	map_id int not null,
+        requested_status varchar(6) default '' not null,
+        comment text default '' not null,
 	player_id int not null,
 	datetime datetime not null,
 	active tinyint(1) not null
