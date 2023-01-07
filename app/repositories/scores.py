5eb3e8b98e3210716f2aa6e0325c0646f0b87608
Josh Smith <cmyuiosu@gmail.com>
2023-01-07 12:32:58 +0000

    style: consistent formatting in repositories

--- app/repositories/scores.py
+++ app/repositories/scores.py
@@ -65,8 +65,14 @@ async def create(
     online_checksum: str,
 ) -> dict[str, Any]:
     query = """\
-        INSERT INTO scores (map_md5, score, pp, acc, max_combo, mods, n300, n100, n50, nmiss, ngeki, nkatu, grade, status, mode, play_time, time_elapsed, client_flags, userid, perfect, online_checksum)
-             VALUES (:map_md5, :score, :pp, :acc, :max_combo, :mods, :n300, :n100, :n50, :nmiss, :ngeki, :nkatu, :grade, :status, :mode, :play_time, :time_elapsed, :client_flags, :userid, :perfect, :online_checksum)
+        INSERT INTO scores (map_md5, score, pp, acc, max_combo, mods, n300,
+                            n100, n50, nmiss, ngeki, nkatu, grade, status,
+                            mode, play_time, time_elapsed, client_flags,
+                            userid, perfect, online_checksum)
+             VALUES (:map_md5, :score, :pp, :acc, :max_combo, :mods, :n300,
+                     :n100, :n50, :nmiss, :ngeki, :nkatu, :grade, :status,
+                     :mode, :play_time, :time_elapsed, :client_flags,
+                     :userid, :perfect, :online_checksum)
     """
     params = {
         "map_md5": map_md5,
