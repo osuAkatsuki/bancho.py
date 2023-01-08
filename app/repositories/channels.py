0ef0156e95309a56c54ba2cc3fca2c45a5c39768
Sebastian Bishop <yoru@its.moe>
2023-01-08 01:55:45 -0600

    style: SQL formatting
    Co-authored-by: Josh Smith <cmyuiosu@gmail.com>

--- app/repositories/channels.py
+++ app/repositories/channels.py
@@ -94,8 +94,8 @@ async def fetch_count(
         SELECT COUNT(*) AS count
           FROM channels
          WHERE read_priv = COALESCE(:read_priv, read_priv)
-            AND write_priv = COALESCE(:write_priv, write_priv)
-            AND auto_join = COALESCE(:auto_join, auto_join)
+           AND write_priv = COALESCE(:write_priv, write_priv)
+           AND auto_join = COALESCE(:auto_join, auto_join)
     """
     params = {
         "read_priv": read_priv,
