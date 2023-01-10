b276969cbfba4c30a8ca6a925a4c6c5856cd2e37
FlyingCat-X <flyingcatdm@gmail.com>
2023-01-09 20:45:23 -0500

    Use host selected gamemode instead of map gamemode

--- app/api/domains/cho.py
+++ app/api/domains/cho.py
@@ -1534,7 +1534,7 @@ class MatchChangeSettings(BasePacket):
                 player.match.map_id = bmap.id
                 player.match.map_md5 = bmap.md5
                 player.match.map_name = bmap.full_name
-                player.match.mode = bmap.mode
+                player.match.mode = player.match.host.status.mode
             else:
                 player.match.map_id = self.match_data.map_id
                 player.match.map_md5 = self.match_data.map_md5
