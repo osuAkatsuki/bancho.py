baad29655ce9beb6ee4f289e0716e0f9f096330f
Josh Smith <cmyuiosu@gmail.com>
2023-01-10 15:10:21 +0000

    refactor: api openapi tags

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter
 from .v1 import apiv1_router
 from .v2 import apiv2_router
 
-api_router = APIRouter(tags=["bancho.py API"])
+api_router = APIRouter()
 
 api_router.include_router(apiv1_router)
 api_router.include_router(apiv2_router)
--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -5,6 +5,6 @@ from fastapi import APIRouter
 from . import players
 
 
-apiv2_router = APIRouter(tags=["API-v2"], prefix="/v2")
+apiv2_router = APIRouter(tags=["API v2"], prefix="/v2")
 
 apiv2_router.include_router(players.router)
