c1f352abdaf271fc9ae0fbb9e98e16be25712c8f
minisbett <eisenerzyt@gmail.com>
2023-01-11 16:04:27 +0100

    comment clarifications

--- app/usecases/anticheat.py
+++ app/usecases/anticheat.py
@@ -3,16 +3,18 @@ from __future__ import annotations
 import inspect
 import sys
 from typing import Optional
-
 import app.logging
 import app.state.sessions
+
+# DO NOT import non-anticheat check functions directly, it'll mess up inspect.getmembers
+# You can create separate .py files containing more checks for a better hierarchy
+# and import them here, e.g. with import example_check from app.usecases.anticheat.pp_caps
+
 from app.constants.gamemodes import GameMode
 from app.constants.mods import Mods
 from app.constants.privileges import Privileges
 from app.objects.player import Player
 from app.objects.score import Score
-
-# DO NOT import non-anticheat check functions directly, it'll mess up inspect.getmembers
 
 
 async def run_anticheat_checks(player: Player, score: Score):
