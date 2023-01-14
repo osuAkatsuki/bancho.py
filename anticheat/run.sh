cf394e9195c62129466ccb3199e5704a0ba068e3
minisbett <eisenerzyt@gmail.com>
2023-01-14 21:13:59 +0100

    Fix run script

--- anticheat/run.sh
+++ anticheat/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 1 ]; then
-    if [ "$1" == "debug" ]; then
+    if [ "$1" = "debug" ]; then
         echo "Running anticheat in Debug configuration."
         dotnet run --configuration Debug
         exit
