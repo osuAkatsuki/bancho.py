60cb6587efc6194657b742d59907225c71232418
arily <arily@arily.moe>
2023-04-17 00:16:36 +0900

    remove status code check

--- app/objects/beatmap.py
+++ app/objects/beatmap.py
@@ -82,15 +82,17 @@ async def ensure_local_osu_file(
 
     url = f"https://old.ppy.sh/osu/{bmap_id}"
     async with app.state.services.http_client.get(url) as resp:
-        if not resp.ok:
-            # client error, report this to cmyui
+        if resp.headers.get("Content-Disposition") is None:
+            # bmap_id is not exists.
+            # considering remove following code as it occurs frequently.
             stacktrace = app.utils.get_appropriate_stacktrace()
             await app.state.services.log_strange_occurrence(stacktrace)
+            return False
 
-        bmap_bytes = await resp.read()
-        bytes_md5 = hashlib.md5(bmap_bytes).hexdigest()
         if bmap_bytes.strip():
             return False
+        bmap_bytes = await resp.read()
+        bytes_md5 = hashlib.md5(bmap_bytes).hexdigest()
         osu_file_path.write_bytes(bmap_bytes)
         if bmap_md5 is not None and bytes_md5 is not bmap_md5:
             return False
