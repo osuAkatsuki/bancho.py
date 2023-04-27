5283b934e8766c2262cd4415f6c0c7fa838f479f
cmyui <cmyuiosu@gmail.com>
2023-04-27 00:50:11 -0400

    fix smtp port type

--- app/settings.py
+++ app/settings.py
@@ -61,7 +61,7 @@ DATADOG_API_KEY = os.environ["DATADOG_API_KEY"]
 DATADOG_APP_KEY = os.environ["DATADOG_APP_KEY"]
 
 SMTP_SERVER = os.environ["SMTP_SERVER"]
-SMTP_PORT = os.environ["SMTP_PORT"]
+SMTP_PORT = int(os.environ["SMTP_PORT"])
 SMTP_USERNAME = os.environ["SMTP_USERNAME"]
 SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
 SMTP_EMAIL = os.environ["SMTP_EMAIL"]
