42fb981e3144728cfa7bdc53cd3aab1b6551ee2e
cmyui <cmyuiosu@gmail.com>
2023-09-25 04:14:53 -0400

    dynamic app_port for nginx install script

--- scripts/install-nginx-config.sh
+++ scripts/install-nginx-config.sh
@@ -7,7 +7,7 @@ source .env
 set +a
 
 echo "Installing nginx configuration"
-envsubst '${DOMAIN},${SSL_CERT_PATH},${SSL_KEY_PATH},${DATA_DIRECTORY}' < ext/nginx.conf.example > /etc/nginx/sites-available/bancho.conf
+envsubst '${APP_PORT},${DOMAIN},${SSL_CERT_PATH},${SSL_KEY_PATH},${DATA_DIRECTORY}' < ext/nginx.conf.example > /etc/nginx/sites-available/bancho.conf
 ln -f -s /etc/nginx/sites-available/bancho.conf /etc/nginx/sites-enabled/bancho.conf
 
 echo "Restarting nginx"
