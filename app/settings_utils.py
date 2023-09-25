b121317b50942a3966829e2162789fa3e24da287
cmyui <cmyuiosu@gmail.com>
2023-09-25 06:25:59 -0400

    better handling for deprecated env vars

--- app/settings_utils.py
+++ app/settings_utils.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 import os
-import warnings
 from datetime import date
+
+from app.logging import Ansi
+from app.logging import log
 
 
 def read_bool(value: str) -> bool:
@@ -13,19 +15,29 @@ def read_list(value: str) -> list[str]:
     return [v.strip() for v in value.split(",")]
 
 
-def support_deprecated_vars(new_name: str, deprecated_name: str, *, until: date) -> str:
-    val = os.getenv(new_name)
-    if val is not None:
-        return val
+def support_deprecated_vars(
+    new_name: str,
+    deprecated_name: str,
+    *,
+    until: date,
+    allow_empty_string: bool = False,
+) -> str:
+    val1 = os.getenv(new_name)
+    if val1:
+        return val1
 
-    val = os.getenv(deprecated_name)
-    if val is not None:
-        warnings.warn(
-            f"{deprecated_name} has been deprecated and will be supported until {until.isoformat()}. Use  {new_name} instead.",
-            DeprecationWarning,
+    val2 = os.getenv(deprecated_name)
+    if val2:
+        log(
+            f"{deprecated_name} has been deprecated and will be supported until {until.isoformat()}. Use {new_name} instead.",
+            Ansi.LYELLOW,
         )
-        return val
+        return val2
 
-    raise KeyError(
-        f"Neither {new_name} nor {deprecated_name} are set in the environment.",
-    )
+    if allow_empty_string:
+        if val1 is not None:
+            return val1
+        if val2 is not None:
+            return val2
+
+    raise KeyError(f"{new_name} is not set in the environment")
