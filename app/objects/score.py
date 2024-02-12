77a1abc4b5fab04f81d1b52a450cd20824d97adf
mini <39670899+minisbett@users.noreply.github.com>
2024-02-12 13:17:29 +0100

    Revert rounding

--- app/objects/score.py
+++ app/objects/score.py
@@ -367,7 +367,7 @@ class Score:
             # both of our score's submission statuses.
             # NOTE: this will be updated in sql later on in submission
             # NOTE: Sync the decimal rounding with the amount of decimals in the scores.pp SQL column
-            if round(self.pp, 3) > rec["pp"]:
+            if self.pp, 3 > rec["pp"]:
                 self.status = SubmissionStatus.BEST
                 self.prev_best.status = SubmissionStatus.SUBMITTED
             else:
