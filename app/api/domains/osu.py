cee1ece30aebab23fb0b71a12e937ad9ff0c5baa
minisbett <39670899+minisbett@users.noreply.github.com>
2024-02-15 16:55:32 +0100

    Moved error to constant

--- app/api/domains/osu.py
+++ app/api/domains/osu.py
@@ -1644,6 +1644,7 @@ async def peppyDMHandler() -> Response:
 
 """ ingame registration """
 
+INGAME_REGISTRATION_DISALLOWED_ERROR = {"form_error": {"user": {"password": ["In-game registration is disabled. Please register on the website."]}}}
 
 @router.post("/users")
 async def register_account(
@@ -1670,15 +1671,7 @@ async def register_account(
     # Disable in-game registration if enabled
     if app.settings.DISALLOW_INGAME_REGISTRATION:
         return ORJSONResponse(
-            content={
-                "form_error": {
-                    "user": {
-                        "password": [
-                            "In-game registration is disabled. Please register on the website.",
-                        ],
-                    },
-                },
-            },
+            content=INGAME_REGISTRATION_DISALLOWED_ERROR,
             status_code=status.HTTP_400_BAD_REQUEST,
         )
 
