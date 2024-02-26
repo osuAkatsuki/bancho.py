34353e89f1c76dcf931d7612ec00cc99b45aa81c
cmyui <cmyuiosu@gmail.com>
2024-02-26 05:18:58 -0500

    fix diffs against table definitions

--- app/repositories/comments.py
+++ app/repositories/comments.py
@@ -1,40 +1,36 @@
 from __future__ import annotations
 
-from enum import StrEnum
 from typing import TypedDict
 from typing import cast
 
 from sqlalchemy import CHAR
 from sqlalchemy import Column
-from sqlalchemy import Enum
 from sqlalchemy import Integer
-from sqlalchemy import String
 from sqlalchemy import and_
 from sqlalchemy import insert
 from sqlalchemy import or_
 from sqlalchemy import select
-from sqlalchemy.dialects.mysql import FLOAT
+from sqlalchemy.dialects.mysql import ENUM
+from sqlalchemy.dialects.mysql import VARCHAR
 
 import app.state.services
 from app.repositories import Base
 from app.repositories.users import UsersTable
 
 
-class TargetType(StrEnum):
-    REPLAY = "replay"
-    BEATMAP = "map"
-    SONG = "song"
-
-
 class CommentsTable(Base):
     __tablename__ = "comments"
 
     id = Column("id", Integer, nullable=False, primary_key=True, autoincrement=True)
-    target_id = Column("target_id", nullable=False)
-    target_type = Column(Enum(TargetType, name="target_type"), nullable=False)
+    target_id = Column("target_id", Integer, nullable=False)
+    target_type = Column(ENUM("replay", "map", "song"), nullable=False)
     userid = Column("userid", Integer, nullable=False)
-    time = Column("time", FLOAT(precision=6, scale=3), nullable=False)
-    comment = Column("comment", String(80, collation="utf8"), nullable=False)
+    time = Column("time", Integer, nullable=False)
+    comment = Column(
+        "comment",
+        VARCHAR(charset="utf8mb3", collation="utf8mb3_general_ci", length=80),
+        nullable=False,
+    )
     colour = Column("colour", CHAR(6), nullable=True)
 
 
@@ -106,15 +102,15 @@ async def fetch_all_relevant_to_replay(
         .where(
             or_(
                 and_(
-                    CommentsTable.target_type == TargetType.REPLAY,
+                    CommentsTable.target_type == TargetType.replay,
                     CommentsTable.target_id == score_id,
                 ),
                 and_(
-                    CommentsTable.target_type == TargetType.SONG,
+                    CommentsTable.target_type == TargetType.song,
                     CommentsTable.target_id == map_set_id,
                 ),
                 and_(
-                    CommentsTable.target_type == TargetType.BEATMAP,
+                    CommentsTable.target_type == TargetType.beatmap,
                     CommentsTable.target_id == map_id,
                 ),
             ),
